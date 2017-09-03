e739f4bc3a901bbfda783d51639ce833602f7b5e
後藤祐一 <gotoh@gotouyuuichinoMacBook-Air.local>
2017-09-03 20:26:31 +0900

    Initial Commit

---
+++ BarcodeReaderFromQiita/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarcodeReaderFromQiita
+//
+//  Created by 後藤祐一 on 2017/09/03.
+//  Copyright © 2017年 後藤祐一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
